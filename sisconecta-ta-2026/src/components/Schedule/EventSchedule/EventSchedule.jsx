f403512854c4b1647fa8eeb9e5dccba8f9b9019c
MarcoTulio2511 <rodriguesmarcotulio79@gmail.com>
2026-03-17 17:12:19 -0300

    Ajuste na programação

--- sisconecta-ta-2026/src/components/Schedule/EventSchedule/EventSchedule.jsx
+++ sisconecta-ta-2026/src/components/Schedule/EventSchedule/EventSchedule.jsx
@@ -20,120 +20,133 @@ const programacao = [
         hora: "09h00",
         titulo: "Abertura Oficial – Cerimonial (Arena Sabiazinho)",
         descricao: `
-         • Apresentação cultural – Mestre Fabinho
-         • Demonstração de halterofilismo paralímpico
-         • Treinador: Weverton Santos
-         • Tayana Medeiros – Ouro Paris 2024 (até 86kg)
-         • Lara Lima – Bronze Paris 2024 (até 41kg)
-         • Inovação no paradesporto
-         • Tiro com arco autônomo para deficientes visuais
-         • Alcedo Medeiros (Arqueiro)
-         • Composição da mesa de autoridades
-         • Hino Nacional
-         • Pronunciamento das autoridades
-         • Celebração do Termo de Adesão ao programa Novo Viver Sem Limites
-         • Paulo Sérgio (Prefeito de Uberlândia)
-         • Isadora Nascimento (Secretária Nacional PcD – MDHC)
-         • Entrega CNPq/MCTI – CINTESP.Br/UFU
-         • Homenagem ao paradesporto de Uberlândia
-         • Futel, Aparu, Cddu, Adevitrim, Adeviudi, Compod, Asul, Sesi e Praia Clube
+• Apresentação cultural – Mestre Fabinho
+• Demonstração de halterofilismo paralímpico
+• Weverton Santos (Treinador)
+• Tayana Medeiros – Ouro Paris 2024
+• Lara Lima – Bronze Paris 2024
+
+• Inovação no paradesporto
+• Tiro com arco autônomo – Alcedo Medeiros
+
+• Composição da mesa de autoridades
+• Hino Nacional
+• Pronunciamento das autoridades
+
+• Celebração do Termo de Adesão – Novo Viver Sem Limites
+• Paulo Sérgio (Prefeito)
+• Isadora Nascimento (MDHC)
+
+• Entrega CNPq/MCTI – CINTESP.Br/UFU
+
+• Homenagem ao paradesporto de Uberlândia
+• Futel, Aparu, Cddu, Adevitrim, Adeviudi, Compod, Asul, Sesi e Praia Clube
         `,
       },
+
       {
         hora: "11h00",
-        titulo:
-          "Lançamento da Pedra Fundamental – Praça da Ciência Socioeducativa",
+        titulo: "Lançamento – Praça da Ciência (Parque do Sabiá)",
         descricao: `
-Projeto do CINTESP.Br/UFU com apoio da:
+Projeto do CINTESP.Br/UFU com apoio de:
 • FUTEL / Prefeitura de Uberlândia
 • SEDES/MCTI
 • FINEP
 
-Local: Mundo da Criança – Parque do Sabiá
-Somente autoridades credenciadas
-Gravação exibida na abertura do dia 21/03
+Local: Parque do Sabiá  
+Somente autoridades  
+Exibição no dia 21/03
         `,
       },
+
       {
         hora: "12h30",
-        titulo:
-          "Lançamento da Pedra Fundamental – POLO.TA (Polo Nacional de Inovação)",
+        titulo: "Lançamento – POLO.TA",
         descricao: `
-Projeto do CINTESP.Br/UFU com apoio da:
+Polo Nacional de Inovação em Produtos Assistivos
+
+Apoio:
 • SEDES/MCTI
 • FINEP
 • Prefeitura de Uberlândia
 
-Local: Campus Glória – UFU
-Somente autoridades credenciadas
-Gravação exibida na abertura do dia 21/03
+Local: Campus Glória – UFU  
+Somente autoridades  
+Exibição no dia 21/03
         `,
       },
+
       {
         hora: "11h00",
-        titulo: "Pitch – Produtos Assistivos da Rede SisAssistiva",
-        descricao: "",
+        titulo: "Pitch – Rede SisAssistiva",
+        descricao: "Apresentação rápida de produtos inovadores.",
       },
+
       {
         hora: "12h30",
-        titulo: "Mostra Interativa de Tecnologias Assistivas",
-        descricao: `
-Exposição e experimentação de produtos da Rede SisAssistiva.
-        `,
+        titulo: "Mostra Interativa",
+        descricao:
+          "Exposição e experimentação de tecnologias assistivas (ver hotsite).",
       },
+
       {
         hora: "12h30",
-        titulo: "Oficinas Interativas de Tecnologias Assistivas",
-        descricao: `
-Atividades práticas com tecnologias assistivas em parceria com empresas.
-        `,
+        titulo: "Oficinas Interativas",
+        descricao:
+          "Atividades práticas com tecnologias assistivas (ver hotsite).",
       },
+
       {
         hora: "12h30",
-        titulo: "Almoço Livre – Praça de Alimentação da Arena",
-        descricao: "Almoço não subsidiado.",
+        titulo: "Almoço Livre",
+        descricao: "Praça de alimentação da arena (não subsidiado).",
       },
+
       {
         hora: "14h00",
-        titulo: "PAINEL 1 – Pesquisa e Inovação nas Empresas Brasileiras",
+        titulo: "PAINEL 1 – Inovação nas Empresas",
         descricao: `
 Mediador:
-• Ferdinando Kun – Líder de Comunidade
+• Ferdinando Kun
 
 Painelistas:
 • Fabiano Alves – Prefeitura de Uberlândia
 • Daniel Almeida Filho – SETEC/MCTI
 • Wadson Ribeiro – FINEP
 • Guilherme Lira – TecAssistiva
+• Babu Belarmino – Colibri Interfaces
         `,
       },
+
       {
         hora: "16h00",
-        titulo: "PAINEL 2 – Inclusão socioprodutiva regional",
+        titulo: "PAINEL 2 – Inclusão Socioprodutiva",
         descricao: `
 Mediadora:
-• Tania Maria de Souza Toledo – Secretaria de Educação
+• Tania Toledo – Sec. Educação
 
 Painelistas:
 • Inácio Arruda – SEDES/MCTI
 • Isadora Nascimento – MDHC
-• Fábio Araújo – Secretaria de Paradesporto
-• Renato Carvalho Fernandes – Prefeito de Araguari
+• Fábio Araújo – Paradesporto
+• Renato Fernandes – Prefeito de Araguari
         `,
       },
+
       {
         hora: "14h00",
-        titulo: "Rodas de Conversa com empresas e startups",
-        descricao:
-          "Interação entre empresas, empreendedores e setores do governo.",
+        titulo: "Rodas de Conversa",
+        descricao: "Interação com empresas, startups e governo.",
       },
+
       {
         hora: "15h00",
-        titulo: "Atendimento FINEP – Coordenadores da Rede SisAssistiva",
-        descricao: "Local: Sala Cintesp.Br",
+        titulo: "Atendimento FINEP",
+        descricao: "Para coordenadores da rede (Sala Cintesp.Br).",
       },
     ],
   },
+
   {
     dia: "21/03",
     titulo: "Estruturação da Rede e Transferência Tecnológica",
@@ -145,98 +158,113 @@ Painelistas:
         titulo: "Recepção",
         descricao: "",
       },
+
       {
         hora: "09h00",
         titulo: "Abertura do Dia – Cerimonial",
         descricao: `
-• Vídeo institucional – POLO.TA e Praça da Ciência
-• Composição da mesa (FINEP, SEDES/MCTI, UFU, SISLAB)
-• Lançamento do site SisAssistiva
+• Vídeo POLO.TA e Praça da Ciência
+• Composição da mesa institucional
+• Lançamento do Site SisAssistiva
 • Lançamento do Mapa SisAssistiva
-• Termo de adesão da Rede SisAssistiva
-• Lançamento do Livro Digital SisAssistiva
-• Discussões estratégicas da rede
+• Termo de adesão da rede
+• Lançamento do Livro Digital
+• Planejamento estratégico da rede
         `,
       },
+
       {
         hora: "10h30",
-        titulo: "PAINEL 3 – Políticas Públicas para Tecnologia Assistiva",
+        titulo: "PAINEL 3 – Políticas Públicas",
         descricao: `
 Mediadora:
-• Ana Marta de Souza – Diretora FEMEC/UFU
+• Ana Marta de Souza – UFU
 
 Expositores:
-• Polliana Tannus Bernardes – Sec. Educação Prata
-• Rodrigo Abreu Machado – Ministério do Esporte
-• Milton Pereira Filho – MCTI
+• Polliana Bernardes – Educação Prata
+• Rodrigo Machado – Ministério do Esporte
+• Milton Pereira – MCTI
 • Polyana Penna – IBICT
 • Janine Peixoto – UFU
-• Paulo Gonçalves Veloso – MPT
+• Paulo Veloso – MPT
+
+Objetivos:
+• Projeto piloto PcD
+• Formação em Tecnologia Assistiva
+• Inclusão produtiva regional
         `,
       },
+
       {
         hora: "10h30",
-        titulo: "Mostra Interativa – Rede SisAssistiva",
-        descricao: "Exposição e experimentação de soluções assistivas.",
+        titulo: "Mostra Interativa",
+        descricao: "Exposição de soluções assistivas.",
       },
+
       {
         hora: "10h30",
-        titulo: "Atendimento FINEP – Rede SisAssistiva",
+        titulo: "Atendimento FINEP",
         descricao: "",
       },
+
       {
         hora: "12h00",
-        titulo: "Almoço Livre – Praça de Alimentação",
-        descricao: "Almoço não subsidiado.",
+        titulo: "Almoço Livre",
+        descricao: "Praça de alimentação (não subsidiado).",
       },
+
       {
         hora: "13h30",
-        titulo: "Oficinas Interativas de Tecnologias Assistivas",
+        titulo: "Oficinas Interativas",
         descricao: "",
       },
+
       {
         hora: "14h00",
         titulo: "PAINEL 4 – Regulamentação e Emprego PcD",
         descricao: `
 Mediadora:
-• Carla Patrícia Guimarães – INT
+• Carla Guimarães – INT
 
 Painelistas:
-• Vivian Oliveira – ANVISA
 • Carlos Azevedo – INMETRO
-• Idari Alves – Prefeitura de Uberlândia
+• Idari Alves – Prefeitura
 • Edison Passafaro – ABNT
-• Lindomar Alves – Empresa Mover
+• Lindomar Santos – Mover
         `,
       },
+
       {
         hora: "16h00",
         titulo: "Intervalo",
         descricao: "",
       },
+
       {
         hora: "16h30",
-        titulo: "PAINEL 5 – Empreendedorismo e Empresas Competitivas",
+        titulo: "PAINEL 5 – Empreendedorismo",
         descricao: `
 Mediadora:
 • Sonia da Costa – SEDES/MCTI
 
 Painelistas:
-• Elder Lima – SEBRAE Minas
-• Rafael Reimann Baptista – BioHub PUCRS
+• Elder Lima – SEBRAE
+• Rafael Baptista – BioHub PUCRS
 • Guilherme Calheiros – EMBRAPII
-• Rogério Marino Franco – SENAI Uberlândia
+• Rogério Franco – SENAI
         `,
       },
+
       {
         hora: "18h00",
         titulo: "Encerramento Oficial",
         descricao: "",
       },
+
       {
         hora: "20h00",
-        titulo: "Confraternização por adesão",
-        descricao: "",
+        titulo: "Confraternização",
+        descricao: "Por adesão.",
       },
     ],
   },
